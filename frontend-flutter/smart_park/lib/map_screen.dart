d8edd03979e13f943647e335a1cd6756ccb9c68f
Thanh Hieu <thanhhieu8304@gmail.com>
2025-12-11 20:15:38 +0700

    feat: custom map appbar

--- frontend-flutter/smart_park/lib/map_screen.dart
+++ frontend-flutter/smart_park/lib/map_screen.dart
@@ -22,10 +22,6 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Smart Park Map'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
       body: GoogleMap(
         onMapCreated: _onMapCreated,
         initialCameraPosition: const CameraPosition(
